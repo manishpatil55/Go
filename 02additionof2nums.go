add97f8faed05c11386761c913c22cc7ed7e60e7
manishpatil55 <manish.patil2004@outlook.com>
2024-01-07 12:25:27 +0530

    hello world

---
+++ 02additionof2nums.go
@@ -0,0 +1,13 @@
+// addition of two numbers already given numbers
+package main
+
+import "fmt"
+
+func main() {
+	num1 := 5
+	num2 := 10
+
+	sum := num1 + num2
+
+	fmt.Println("The sum of", num1, "&", num2, "is: ", sum)
+}
