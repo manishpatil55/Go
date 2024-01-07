ed80fff824ca0965bf419ca18c2caa3442a10abb
manishpatil55 <manish.patil2004@outlook.com>
2024-01-07 12:37:29 +0530

    hello world

--- 01helloworld.go
+++ 01helloworld.go
@@ -5,34 +5,3 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
-// addition of two numbers already given numbers
-/*package main
-
-import "fmt"
-
-func main() {
-	num1 := 5
-	num2 := 10
-
-	sum := num1 + num2
-
-	fmt.Println("The sum of", num1, "&", num2, "is: ", sum)
-}/*
-/*
-// addition of two numbers take input from user
-package main
-
-import "fmt"
-
-func main() {
-	var num1, num2 int
-
-	fmt.Println("Enter first number:")
-	fmt.Scanln(&num1)
-	fmt.Println("Enter second number:")
-	fmt.Scanln(&num2)
-
-	sum := num1 + num2
-	fmt.Println("The sum is: ", sum)
-}
-/*
