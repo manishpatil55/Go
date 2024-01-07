9a570ceabd58f0e8d261b2bd951ee65b0198b514
manishpatil55 <manish.patil2004@outlook.com>
2024-01-07 12:42:59 +0530

    addition of 2 nums input from user

---
+++ 02additionof2num.go
@@ -0,0 +1,17 @@
+// addition of two numbers take input from user
+
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2 int
+
+	fmt.Println("Enter first number:")
+	fmt.Scanln(&num1)
+	fmt.Println("Enter second number:")
+	fmt.Scanln(&num2)
+
+	sum := num1 + num2
+	fmt.Println("The sum is: ", sum)
+}
